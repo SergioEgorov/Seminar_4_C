4bda5c7d897fde667c67c9c9cb1fa981de58a30d
Sergey <hojalata@mail.ru>
2022-08-15 20:40:57 +0300

    Решенеи первой задачи не занятии ease enter the commit message for your changes. Lines starting

---
+++ Example_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = Convert.ToInt32(Console.ReadLine()); //[1;A]
+
+int GetSumNumbers(int number)
+{
+    int sum = 0; // Результат
+    int start = 1; // начало нашего цикла
+    while (start <= number) // [1;A]
+    {
+        sum += start; // sum = sum + start
+        start++; // start = start + 1
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма чисел от 1 до {A} = {GetSumNumbers(A)}");
